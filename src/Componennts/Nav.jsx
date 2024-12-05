d81e5dc2172d17c94e1334fca6401142c1756300
udaykosta12 <udaykushvanshi12@gmail.com>
2024-12-05 11:46:33 +0530

    reactBasicProject

---
+++ src/Componennts/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <h1>LOGO</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
