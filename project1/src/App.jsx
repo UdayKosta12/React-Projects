bebcc526b275be67d5a78f1ac47246b220de1eeb
udaykosta12 <udaykushvanshi12@gmail.com>
2024-12-05 10:53:35 +0530

    react-project

---
+++ project1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Nav from './Componennts/Nav'
+import Hero from './Componennts/Hero';
+import Footer from './Componennts/Footer';
+ 
+function App() {
+    return ( 
+    <>
+    <Nav/>
+    <Hero/>
+    <Footer/>
+</>
+);
+}
+
+export default App;
